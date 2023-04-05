7cdd7231fe38f18be9570324baf06c85f7da61b3
fitz95 <fitzgeraldnsong@gmail.com>
2023-04-05 13:52:42 +0100

    add any Method

--- enumerable.rb
+++ enumerable.rb
@@ -5,5 +5,11 @@ module Enumerable
       each {|e| result = false unless block.call(e)}
       result
     end
+
+    def any?(&block)
+        result = false
+        each {|e| result = true unless block.call(e)}
+        result
+      end
 end
-  
+
